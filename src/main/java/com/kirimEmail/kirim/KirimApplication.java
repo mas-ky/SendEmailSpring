48facc8b468da69c9fe728c9c5fc9d4e8c917b45
mas-ky <keymas702@gmail.com>
2023-01-19 16:06:47 +0700

    Send Email from spring

---
+++ src/main/java/com/kirimEmail/kirim/KirimApplication.java
@@ -0,0 +1,13 @@
+package com.kirimEmail.kirim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirimApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kirimEmail/kirim/controller/kirimEmailController.java
@@ -0,0 +1,30 @@
+package com.kirimEmail.kirim.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class kirimEmailController {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+    
+    @GetMapping(value = "/") // @RequestMapping using in the method level ,it has default GET method
+	public ModelAndView dashboard() {
+		ModelAndView model = null;
+		model = new ModelAndView("admin_candidate");
+		System.err.println("eror");
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("ricky.fardiansha@gmail.com","muhammad.dawud91@gmail.com");
+
+        msg.setSubject("Testing from Spring Boot");
+        msg.setText(" Silahkan Hadir  http://recruit.pungkookid.com/adminHome ");
+        javaMailSender.send(msg);
+		return model;
+	}
+}
+ 
